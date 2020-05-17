b3301590afbbc5d18d0f21b832d2a46d43b24381
ehom <erwin.hom@puppet.com>
2020-05-17 14:51:40 -0700

    init check-in of set up script for nltk builds

---
+++ nltk/configure.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import nltk
+
+# ntlk.download()
+
+document = "The quick fox jumped over the lazy brown dog."
+
+print("hello, nltk")
+
+tokens = nltk.word_tokenize(document)
+
+print(tokens)
+
