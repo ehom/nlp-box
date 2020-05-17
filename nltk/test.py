c942fe3c51d675ab44f7baba2e39ab6feffabd64
ehom <ehom@users.noreply.github.com>
2020-05-17 15:11:16 -0700

    add test to check NLTK installation

---
+++ nltk/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import nltk
+
+document = "The quick fox jumped over the lazy brown dog."
+
+print("hello, nltk")
+
+tokens = nltk.word_tokenize(document)
+
+print(tokens)
