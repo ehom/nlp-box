dc3a657c6d90fc351b573ca017e1ba043367709e
ehom <ehom@users.noreply.github.com>
2020-05-17 15:18:51 -0700

    Update NLTK environment setup

--- nltk/configure.py
+++ nltk/configure.py
@@ -3,3 +3,19 @@
 import nltk
 
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
+# from nltk.book import *
+# ^^^^^^^^^^^^^^^^^^^^^^^
+# This doesn't work. Instead, download each book as follows.
+
+nltk.download('gutenberg')
+nltk.download('genesis')
+nltk.download('inaugural')
+nltk.download('nps_chat')
+nltk.download('webtext')
+nltk.download('treebank')
+
+nltk.download('tagsets')
