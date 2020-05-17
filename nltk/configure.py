06edb42630635fe0a7d33ba0b70246884e864021
ehom <ehom@users.noreply.github.com>
2020-05-17 15:11:59 -0700

    remove test code

--- nltk/configure.py
+++ nltk/configure.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
 
 import nltk
+
 nltk.download('punkt')
-
-document = "The quick fox jumped over the lazy brown dog."
-
-print("hello, nltk")
-
-tokens = nltk.word_tokenize(document)
-
-print(tokens)
-
