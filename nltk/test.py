adaf431cdbdf45b1c6b3d2a2109d3131a7fee3b9
ehom <erwin.hom@puppet.com>
2020-05-18 15:49:53 -0700

    Added NlpDoc class

--- nltk/test.py
+++ nltk/test.py
@@ -9,6 +9,13 @@ def separator():
     print("~" * 20)
 
 
+class NlpDoc:
+    def __init__(self, doc):
+        self.text = doc
+        self.tokens = nltk.word_tokenize(self.text.lower())
+        self.tagged = nltk.pos_tag(self.tokens)
+
+
 separator()
 print("Python version is {}".format(sys.version))
 print("NLTK version is {}".format(nltk.__version__))
@@ -17,14 +24,13 @@ separator()
 document = "The quick fox jumped over the lazy brown dog."
 print("Text: {}".format(document))
 
+nlp_doc = NlpDoc(document)
 
 separator()
-tokens = nltk.word_tokenize(document.lower())
 print("Tokens:")
-pp(tokens)
+pp(nlp_doc.tokens)
 separator()
 
 print("POS Tags:")
-tagged = nltk.pos_tag(tokens)
-pp(tagged)
+pp(nlp_doc.tagged)
 separator()
