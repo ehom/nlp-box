81398ba65bcbfa355797c86a0aedff626363e10f
ehom <erwin.hom@puppet.com>
2020-05-17 18:19:15 -0700

    updated script to test workflow trigger

--- spaCy/test.py
+++ spaCy/test.py
@@ -10,3 +10,13 @@ doc = nlp("She ate the pizza")
 
 for token in doc:
     print(token.text, token.pos_, token.dep_, token.head.text)
+
+print('~' * 20)
+
+# Process some text
+doc = nlp(u"Apple is looking at buying U.K. startup for $1 billion")
+
+# Iterate over the predicted entities
+for ent in doc.ents:
+    # Print the entity text and its label
+    print(ent.text, ent.label_)
