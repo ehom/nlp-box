f5d60ccb5692f949340520afd02ef1ceb6c36ac9
ehom <erwin.hom@puppet.com>
2020-05-17 22:06:42 -0700

    Added more calls to nltk

--- nltk/test.py
+++ nltk/test.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
 import nltk
+from pprint import pprint as pp
 
 document = "The quick fox jumped over the lazy brown dog."
 
-print("hello, nltk")
+def separator():
+    print("~" * 20)
+
+separator()
+print("Text: {}".format(document))
 
 tokens = nltk.word_tokenize(document)
 
-print(tokens)
+separator()
+print("Tokens:")
+pp(tokens)
+separator()
+
+print("POS Tags:")
+tagged = nltk.pos_tag(tokens)
+pp(tagged)
+separator()
+
