0085085665f17621ef2c6f523ab6b01aed2bb4b3
ehom <erwin.hom@puppet.com>
2020-05-17 19:09:32 -0700

    Add more code

--- spaCy/test.py
+++ spaCy/test.py
@@ -4,6 +4,8 @@ import spacy
 
 nlp = spacy.load("en_core_web_sm")
 
+print('~' * 21)
+
 doc = nlp("She ate the pizza")
 
 # Predicting Syntactic Dependencies
@@ -15,6 +17,18 @@ print('~' * 21)
 
 # Process some text
 doc = nlp(u"Apple is looking at buying U.K. startup for $1 billion")
+
+# Iterate over the predicted entities
+for ent in doc.ents:
+    # Print the entity text and its label
+    print(ent.text, ent.label_)
+
+print('~' * 21)
+
+text = "It’s official: Apple is the first U.S. public company to reach a $1 trillion market value"
+
+# Process the text
+doc = nlp(text)
 
 # Iterate over the predicted entities
 for ent in doc.ents:
